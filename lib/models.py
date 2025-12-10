f2a0e15beee3ae5f88ffefcf5d6e90aaa7205f95
George <gnm7208@gmail.com>
2025-12-10 16:50:42 +0300

    initial commit

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Boolean
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,6 +10,30 @@ metadata = MetaData(naming_convention=convention)
 Base = declarative_base(metadata=metadata)
 
 
+class Audition(Base):
+    __tablename__ = 'auditions'
+    
+    id = Column(Integer(), primary_key=True)
+    actor = Column(String())
+    location = Column(String())
+    phone = Column(Integer())
+    hired = Column(Boolean())
+    role_id = Column(Integer(), ForeignKey('roles.id'))
+    
+    role = relationship('Role', backref=backref('auditions', lazy=True))
+    
+    def __repr__(self):
+        return f'<Audition {self.actor} for role {self.role_id}>'
+    
+    def role(self):
+        """Returns an instance of role associated with this audition."""
+        return self.role
+    
+    def call_back(self):
+        """Changes the hired attribute to True."""
+        self.hired = True
+
+
 class Role(Base):
     __tablename__ = 'roles'
     
@@ -19,3 +42,27 @@ class Role(Base):
     
     def __repr__(self):
         return f'<Role {self.character_name}>'
+    
+    def auditions(self):
+        """Returns all of the auditions associated with this role."""
+        return self.auditions
+    
+    def actors(self):
+        """Returns a list of names from the actors associated with this role."""
+        return [audition.actor for audition in self.auditions]
+    
+    def locations(self):
+        """Returns a list of locations from the auditions associated with this role."""
+        return [audition.location for audition in self.auditions]
+    
+    def lead(self):
+        """Returns the first instance of the audition that was hired for this role
+        or returns a string 'no actor has been hired for this role'."""
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        return hired_auditions[0] if hired_auditions else 'no actor has been hired for this role'
+    
+    def understudy(self):
+        """Returns the second instance of the audition that was hired for this role
+        or returns a string 'no actor has been hired for understudy for this role'."""
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        return hired_auditions[1] if len(hired_auditions) > 1 else 'no actor has been hired for understudy for this role'
